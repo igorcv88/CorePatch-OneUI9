f24d9c505ec80c0e9dbbe51890cfc2226911bffe
Igor <igorv88@gmail.com>
2026-09-14 05:16:19 -0300

    Make AssetManager hook fail-soft on Android 17

--- app/src/main/java/org/lsposed/corepatch/hook/AssetManagerHook.kt
+++ app/src/main/java/org/lsposed/corepatch/hook/AssetManagerHook.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Build
 import org.lsposed.corepatch.Config
 import org.lsposed.corepatch.XposedHelper.hookBefore
-import org.lsposed.corepatch.XposedHelper.hostClassLoader
 
 object AssetManagerHook : BaseHook() {
     override val name = "AssetManagerHook"
@@ -13,18 +12,20 @@ object AssetManagerHook : BaseHook() {
     override fun hook() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) return
 
-        val assetManagerClazz = hostClassLoader.loadClass("android.content.res.AssetManager")
+        val assetManagerClazz =
+            findClassOrNull("android.content.res.AssetManager") ?: return
 
-        // Targeting R+ (version " + Build.VERSION_CODES.R + " and above) requires"
-        // + " the resources.arsc of installed APKs to be stored uncompressed"
-        // + " and aligned on a 4-byte boundary
-        // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r48:frameworks/base/core/java/android/content/res/AssetManager.java;l=828
-        // public boolean containsAllocatedTable()
-        val containsAllocatedTableMethod =
-            assetManagerClazz.getDeclaredMethod("containsAllocatedTable")
-        hookBefore(containsAllocatedTableMethod) { callback ->
-            if (Config.isBypassResourceArscRestrictionsEnabled()) {
-                callback.returnAndSkip(false)
+        val containsAllocatedTableMethod = findMethodOrNull(
+            assetManagerClazz,
+            "containsAllocatedTable",
+        ) { m -> m.name == "containsAllocatedTable" && m.parameterCount == 0 }
+            ?: return
+
+        compat("hook containsAllocatedTable") {
+            hookBefore(containsAllocatedTableMethod) { callback ->
+                if (Config.isBypassResourceArscRestrictionsEnabled()) {
+                    callback.returnAndSkip(false)
+                }
             }
         }
     }
